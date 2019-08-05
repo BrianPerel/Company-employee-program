79592b9d2441ce753f00b7625389da3097be10f5
Brian Perel <brianperel@comcast.net>
2019-08-05 15:15:30 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,7 +1,8 @@
 '''
 Author @ Brian Perel
-HW7 - GUI Employee Management System
--> Python program that will store information\nabout employees in a company using a dictionary 
+GUI Employee Management System
+-> Python program that will store information\nabout employees in a company
+using a dictionary. Uses an employee class to set and get employee attributes. 
 '''
 
 import tkinter as tk 
@@ -11,15 +12,34 @@ import mysql.connector
 
 class MyGUI:
     def __init__(self):
+        ''' create and place main gui window, buttons, labels, entry's, canvas line '''
         #print(__doc__)
         self.main_window = tk.Tk() # make the GUI window
         self.main_window.geometry('520x345') # width x height
         self.main_window.configure(background='lightgrey')
         self.main_window.title('Company')
-        
-       
+
+        try:
+            self.mydb = mysql.connector.connect(
+                host='localhost', user='root', passwd='', database='employee_db')
+
+
+        except Exception:
+            self.mydb = mysql.connector.connect(
+                host='localhost', user='root', passwd='')
+
+        self.mycursor = self.mydb.cursor()
+        self.mycursor.execute('CREATE DATABASE IF NOT EXISTS employee_db')
+        self.mycursor.execute('use employee_db')
+        self.mycursor.execute('CREATE TABLE IF NOT EXISTS employees (id INT, \
+                            name VARCHAR(30), dept VARCHAR(30),\
+                            title VARCHAR(30), pay_rate FLOAT(10,2), \
+                            phone_number INT, \
+                            work_type VARCHAR(30))')
+
         # create a GUI label (display EMPLOYEE MANAGEMENT SYSTEM) = the header of the GUI app
-        self.header = tk.Label(text = 'EMPLOYEE MANAGEMENT SYSTEM', font = 'Times 12 bold', bg='lightgrey')
+        self.header = tk.Label(text = 'EMPLOYEE MANAGEMENT SYSTEM',
+                               font = 'Times 12 bold', bg='lightgrey')
                 
         # GUI button 1
         self.my_button1 = tk.Button(text = 'Look Up Employee', \
@@ -39,7 +59,8 @@ class MyGUI:
         
 
         # GUI button
-        self.my_button2 = tk.Button(text = 'Add Employee', font = 'Courier 10', \
+        self.my_button2 = tk.Button(text = 'Add New Employee',
+                            font = 'Courier 10', \
                             command = self.add_employee)
 
         self.label2 = tk.Label(text = '\tEmployee Name:', font = 'Courier 10', \
@@ -77,11 +98,11 @@ class MyGUI:
         self.output_entry_var3 = tk.StringVar()
 
         self.output_entry3 = tk.Entry(width = 20, \
-                                           textvariable = self.output_entry_var3)
+                                    textvariable = self.output_entry_var3)
 
 
         self.label5 = tk.Label(text = '\tPay Rate:', font = 'Courier 10', \
-                                                               bg='lightgrey')
+                                                        bg='lightgrey')
 
         self.output_entry_var4 = tk.StringVar()
 
@@ -89,22 +110,22 @@ class MyGUI:
                                     textvariable = self.output_entry_var4)
 
         self.label6 = tk.Label(text = '\tPhone Number:', font = 'Courier 10', \
-                                                               bg='lightgrey')
+                                                        bg='lightgrey')
 
         self.output_entry_var5 = tk.StringVar()
 
         self.output_entry5 = tk.Entry(width = 20, \
                                     textvariable = self.output_entry_var5)
 
-        self.cb_var1 = tk.IntVar()
-        self.cb_var1.set(0)
-        self.cb1 = tk.Checkbutton(text='Part Time Employee', variable=self.cb_var1, \
-                                    bg='lightgrey')
+        self.radio_var = tk.IntVar()
+        self.radio_var.set(0)
+        
+        self.rb1 = tk.Radiobutton(text='Part Time Employee', variable=self.radio_var, \
+                                    bg='lightgrey', value=1)
 
-        self.cb_var2 = tk.IntVar()
-        self.cb_var2.set(0)
-        self.cb2 = tk.Checkbutton(text='Full Time Employee', variable=self.cb_var2, \
-                                    bg='lightgrey')
+        self.rb2 = tk.Radiobutton(text='Full Time Employee', variable=self.radio_var, \
+                                    bg='lightgrey', value=2)
+
         
 
         #GUI button
@@ -113,69 +134,48 @@ class MyGUI:
         
 
         self.quit_button = tk.Button(text='Quit Program', font = 'Courier 10', \
-                                          command=self.main_window.destroy)
+                                          command=quit)
 
         self.canvas = tk.Canvas(self.main_window, width=495, height=40, bd=0, \
-                                borderwidth=0, bg='lightgrey', highlightthickness=0.5, \
-                                highlightbackground='lightgrey')
+                            borderwidth=0, bg='lightgrey', highlightthickness=0.5, \
+                            highlightbackground='lightgrey')
 
         self.canvas.create_line(2, 25, 800, 25)
 
         # make program position and display
         self.header.place(x = 120, y = 0)
-
         self.my_button1.place(x = 10, y = 65)
-        
-        # make program position and display 
         self.label1.place(x = 203, y = 67)
-        
-        self.output_entry.place(x = 380, y = 67)
-        
+        self.output_entry.place(x = 380, y = 67)        
         self.my_button2.place(x = 10, y = 105)
-        
         self.label2.place(x = 186, y = 107)
-        
         self.output_entry1.place(x = 380, y = 107)
-        
         self.my_button3.place(x = 10, y = 145)
-        
         self.label3.place(x = 187, y = 147)
-        
         self.output_entry2.place(x = 380, y = 147)
-        
         self.my_button4.place(x = 10, y = 185)
-
         self.label4.place(x = 180, y = 187)
-        
         self.output_entry3.place(x = 380, y = 187)
-
         self.label5.place(x = 230, y = 225)
-    
         self.output_entry4.place(x = 380, y = 227)
-        
         self.label6.place(x = 200, y = 265)
-        
         self.output_entry5.place(x = 380, y = 267)
-        
-        self.cb1.place(x = 240, y = 305)
-
-        self.cb2.place(x = 380, y = 305)
-        
+        self.rb1.place(x = 240, y = 305)
+        self.rb2.place(x = 380, y = 305)
         self.reset_button.place(x = 10, y = 225)
-
         self.quit_button.place(x = 10, y = 265)
-    
         self.canvas.place(x = 10, y = 20)
        
 
-
 # App operations: 
 
-    # create an empty dictionary
+    # create the empty dictionary
     employees = {}
 
 
     def look_up_employee(self):
+        ''' function to look up an employee's info in dictionary,
+        by the ID attained from GUI '''
         # Get an employee ID number to look up.
         ID = self.output_entry.get()
 
@@ -190,24 +190,15 @@ class MyGUI:
         self.output_entry_var.set(''), self.output_entry_var1.set('')
         self.output_entry_var2.set(''), self.output_entry_var3.set('')
         self.output_entry_var4.set(''), self.output_entry_var5.set('')
-        
 
-    def db_add(self, ID, name, dept, title, pay_rate, phone_number, work_type):
-        mydb = mysql.connector.connect(host='localhost', user='root', passwd='')
-        mycursor = mydb.cursor()
-        mycursor.execute('CREATE DATABASE IF NOT EXISTS employee_db')
-
-        mycursor.execute('use employee_db')
-        mycursor.execute('CREATE TABLE IF NOT EXISTS employees (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), dept VARCHAR(30), title VARCHAR(30), pay_rate FLOAT(4,2), phone_number INT, work_type VARCHAR(30))')
-
-        sql = 'INSERT INTO employees (id, name, dept, title, pay_rate, phone_number, work_type) values (%s, %s, %s, %s, %s, %s, %s)'
-        val = (ID, name, dept, title, pay_rate, phone_number, work_type)
-        mycursor.execute(sql, val)
-        mydb.commit()
-        print(mycursor.rowcount, 'record inserted')
-        
+        sql = "SELECT * FROM employees WHERE id = %s"
+        self.mycursor.execute(sql, (ID,))
+        display = self.mycursor.fetchall()
+        for data in display:
+            print(data)
             
     def add_employee(self):
+        ''' function to add an employee to dictionary, by info gathered from GUI '''
         # get values from entry box widget
         check = True
         message = ''
@@ -220,48 +211,66 @@ class MyGUI:
             title = self.output_entry3.get()
             pay_rate = self.output_entry4.get()
             phone_number = self.output_entry5.get()
-            
+
+            # ID, pay_rate, and phone_number should be given as strings
             int(ID)
+            int(pay_rate)
+            int(phone_number)
             
             
-        except ValueError:
+        except ValueError as err:
             check = False 
             tk.messagebox.showinfo('Info', 'Error!')
 
-        if self.cb_var1.get() == 1:
+        if self.radio_var.get() == 0:
+            tk.messagebox.showinfo('Info', 'Error!')
+        elif self.radio_var.get() == 1:
             work_type = 'Part time'
-        elif self.cb_var2.get() == 1:
+        elif self.radio_var.get() == 2:
             work_type = 'Full time'
 
-
         # create instance and send the values 
-        new_emp = Employee_Management_System.Employee(name, ID, dept, title, pay_rate, phone_number, work_type)
+        new_emp = Employee_Management_System.Employee(
+                    name, ID, dept, title, pay_rate, phone_number, work_type)
 
-        if ID not in self.employees and check and len(ID) == 6 and name != '' and dept != '' and title != '' and pay_rate != '' and phone_number != '':
-            self.employees[ID] = new_emp
-            message = 'The new employee has been added'
-        elif check == False:
-            message = 'Could not add employee!\nEmployee ID must be digit format'
-        elif len(ID) < 6 or len(ID) > 6:
-            message = 'Could not add employee!\nEmployee ID must be 6 digits'
-        elif name == '' or dept == '' or title == '' or pay_rate == '' or phone_number == '':
-            message = 'Could not add employee.\nMissing data!'
+        self.mycursor.execute('CREATE TABLE IF NOT EXISTS employees (id INT, \
+                            name VARCHAR(30), dept VARCHAR(30), \
+                            title VARCHAR(30), pay_rate FLOAT(10,2), \
+                            phone_number INT, work_type VARCHAR(30))')
+
 
         
+        if ID not in self.employees and check and len(ID) == 6 and name != '' \
+           and dept != '' and title != '' and pay_rate != '' \
+           and phone_number != '' and work_type != '':
+            self.employees[ID] = new_emp
+            message = 'The new employee has been added'
+            tk.messagebox.showinfo('Info', message)
+
+            sql = 'INSERT INTO employees (id, name, dept, title, \
+            pay_rate, phone_number, work_type) values (%s, %s, %s, %s, %s, %s, %s)'
+            
+            val = (ID, name, dept, title, pay_rate, phone_number, work_type)
+            self.mycursor.execute(sql, val)
+            self.mydb.commit()
+            print(self.mycursor.rowcount, 'record inserted')
+        elif ID == '' or name == '' or dept == '' or title == '' \
+             or pay_rate == '' or phone_number == '' or work_type == '' \
+             or check == False or len(ID) < 6 or len(ID) > 6:
+            message = 'Error! Could not add employee.'
         elif ID in self.employees:
             message = 'An employee with that ID already exists'
 
-        self.db_add(ID, name, dept, title, pay_rate, phone_number, work_type)
-           
-        tk.messagebox.showinfo('Info', message)
         
         # set all entry widgets to a blank value
         self.output_entry_var.set(''), self.output_entry_var1.set('')
         self.output_entry_var2.set(''), self.output_entry_var3.set('')
         self.output_entry_var4.set(''), self.output_entry_var5.set('')
-        self.cb_var1.set(0), self.cb_var2.set(0)
+        self.radio_var.set(0)
 
     def update_employee(self):
+        ''' function to update an already existing employee's info
+            in dictionary, by attaining info from GUI '''
         message = ''
         check = True 
         # get values from entry box widget
@@ -269,7 +278,7 @@ class MyGUI:
             ID = self.output_entry.get()
             int(ID)
 
-        except ValueError:
+        except ValueError as err:
             check = False
        
         if ID in self.employees:
@@ -277,15 +286,24 @@ class MyGUI:
             title, pay_rate = self.output_entry3.get(), self.output_entry4.get()
             phone_number = self.output_entry5.get()
 
-            if self.cb_var1.get() == 1:
+            if self.radio_var.get() == 0:
+                tk.messagebox.showinfo('Info', 'Error!')
+            elif self.radio_var.get() == 1:
                 work_type = 'Part time'
-            elif self.cb_var2.get() == 1:
+            elif self.radio_var.get() == 2:
                 work_type = 'Full time'
 
             new_emp = Employee_Management_System.Employee(name, ID, dept, \
-                                                    title, pay_rate, phone_number, work_type)
+                                    title, pay_rate, phone_number, work_type)
             
             self.employees[ID] = new_emp
+
+            sql = 'UPDATE employees SET name=%s, dept=%s, title=%s, \
+            pay_rate=%s, phone_number=%s, work_type=%s WHERE id=%s'
+            val = ((name,), (dept,), (title,), (pay_rate,), (phone_number,), (work_type,), (ID,))
+            self.mycursor.execute(sql, val)
+            self.mydb.commit()
+            print(self.mycursor.rowcount, 'record(s) updated')
             
             message = 'The new employee has been updated'
 
@@ -294,25 +312,35 @@ class MyGUI:
         elif ID not in self.employees:
             message = 'No employee found of this ID'
 
+
         tk.messagebox.showinfo('Info', message)
 
         # set all entry widgets to a blank value
         self.output_entry_var.set(''), self.output_entry_var1.set('')
         self.output_entry_var2.set(''), self.output_entry_var3.set('')
         self.output_entry_var4.set(''), self.output_entry_var5.set('')
-        self.cb_var1.set(0), self.cb_var2.set(0)
+        self.radio_var.set(0)
 
         
     def delete_employee(self):
+        ''' function to delete an employee from app, by locating it
+            by ID in dictionary '''
         # get values from entry box widget 
         ID = self.output_entry.get()
+
+                
+        sql = "DELETE FROM employees WHERE id = %s"
+        self.mycursor.execute(sql, (ID,))
+        self.mydb.commit()
+        print(self.mycursor.rowcount, 'record(s) deleted')
 
         if ID in self.employees:
             del self.employees[ID]
             message = 'Employee information deleted'
         else:
             message = 'The specified ID number was not found'
-
+            
+        
         tk.messagebox.showinfo('Info', message)
         
         # set all entry widgets to a blank value
@@ -321,15 +349,13 @@ class MyGUI:
         self.output_entry_var4.set(''), self.output_entry_var5.set('')
 
     def reset_system(self):
+        ''' function to reset app data, in case company leaves.
+            This will delete all data in app and database ''' 
         self.employees = {}
-
-        mydb = mysql.connector.connect(host='localhost', user='root', passwd='')
-        mycursor = mydb.cursor()
+        
         try:
-            mycursor.execute('DROP DATABASE employee_db')
-            tk.messagebox.showinfo('Info', 'System has been reset, database deleted')
-
-
+            self.mycursor.execute('DROP TABLE employees')
+            tk.messagebox.showinfo('Info', 'System has been reset, table deleted')
         except mysql.connector.Error as err:
             tk.messagebox.showinfo('Info', 'Error, database not found')            
 
@@ -337,5 +363,5 @@ class MyGUI:
         self.output_entry_var.set(''), self.output_entry_var1.set('')
         self.output_entry_var2.set(''), self.output_entry_var3.set('')
         self.output_entry_var4.set(''), self.output_entry_var5.set('')
- 
+                
 my_gui = MyGUI()
